211e9d7e301abe51bac6b8ba589e0ab7a5d3563b
Akepati9 <akepati.prashanti@gmail.com>
2022-01-16 06:33:11 +0000

    first commit

---
+++ git_hellow_world.java
@@ -0,0 +1,13 @@
+package git_project;
+
+public class git_hellow_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("git hello world");
+		
+		
+
+	}
+
+}
